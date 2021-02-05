bd5070f69cd14704379ed0f2dfbd8b6a4a16a4cc
halderdipam <43072215+halderdipam@users.noreply.github.com>
2021-02-05 21:15:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-
+def namelist(names):
+    nam=[i['name'] for i in names]
+    if len(nam) <= 1:
+        return ' '.join(nam)
+    else: 
+        m = ', '.join(map(str,nam[:-1]))
+        res = m + ' & '+ nam[-1]
+        return res
